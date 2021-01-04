271a5a91db1d8f43f9f3a9ec0261c96a910cf7b8
Adrian Marquis <adrian.marquis78@gmail.com>
2021-01-03 21:10:48 -0500

    revision requests handled

--- src/bio/bio.js
+++ src/bio/bio.js
@@ -5,10 +5,12 @@ export default function Bio() {
     return (
         <div>
             <p>I am a student in Thinkful’s Engineering FLEX program. I pride myself on thoroughly learning terminology, 
-            concepts, methods, and application. Analyzing others’ projects has been instrumental in creating my own 
+            concepts, methods, and application. Interacting with others’ projects has been instrumental in creating my own 
             internal database of what clean dynamic code should look like utilizing meticulous syntax. I take a 
             minimalist approach in HTML, CSS, and JavaScript believing that change tolerance and readability makes 
-            later revision far easier. I am perpetually learning, always malleable.
+            later revision far easier. I have found some of my best learning attained by mutually pursuing educational 
+            milestones with others in my cohort and meeting people through the immense digital communities of Github, Slack, and 
+            others offered through Thinkful.
             <br></br>
             <br></br>
             I'm a recreation seeker in the wide outdoor world. Mountains, oceans, cycling, kayaking, boomeranging, 
