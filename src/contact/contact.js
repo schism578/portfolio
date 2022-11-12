65f111bda0008ea9e576ad808aea9f71e8106da7
Adrian Marquis <adrian.marquis78@gmail.com>
2022-11-12 09:25:47 -0500

    nav menu width increase

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -4,7 +4,7 @@ import classes from './contact.module.css';
 const Contact = () => {
     return (
         <div className={classes.Contact}>
-            <span><a href='mailto:adrian.marquis78@gmail.com' className='contact-link' target='_blank'rel='noopener noreferrer'><i class="fas fa-envelope"></i></a></span>
+            <span><a href='mailto:ajmarquis3@gmail.com' className='contact-link' target='_blank'rel='noopener noreferrer'><i class="fas fa-envelope"></i></a></span>
             <span><a href='https://github.com/schism578' className='contact-link' target='_blank' rel='noopener noreferrer'><i class="fab fa-github-square"></i></a></span>
             <span><a href='https://linkedin.com/in/amarquis3' className='contact-link' target='_blank' rel='noopener noreferrer'><i class="fab fa-linkedin"></i></a></span>
         </div>
